1a2762a09df69e680784f58dc9a206714f174abf
juseungjuseung <juseungjuseung@gmail.com>
2026-07-27 20:43:14 +0900

    feat: 로컬환경 및 운영환경 BASE_URL 분기처리

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080/";
+export const BASE_URL = import.meta.env.DEV ? "http://localhost:8080/" : "/api/";
 
 function getCookie(name) {
   const matches = document.cookie.match(
